13d5a38d52cc5909e3b2e75a9a6ea78f504fa44f
Philippe Lafontaine <lafontaine.philippe@gmail.com>
2022-03-18 15:57:08 -0400

    chore: remove extern folder and add vcpkg

--- cmake/WorkspaceEnvironment.cmake
+++ cmake/WorkspaceEnvironment.cmake
@@ -3,6 +3,7 @@ include_guard()
 
 # Global environment setup
 include(CMakeDependentOption)
+include(FetchContent)
 include(WorkspaceHelpers)
 
 # Globals
@@ -10,3 +11,8 @@ set(WORKSPACE_PACKAGE_NAME Wordle)
 string(TOLOWER ${WORKSPACE_PACKAGE_NAME}_workspace WORKSPACE_NAME)
 set(WORKSPACE_PACKAGE_VERSION "0.1.0" CACHE STRING "Current project version")
 set(WORKSPACE_TEST_SUFFIX_REGEX "([-_]t|T)ests?$")
+
+FetchContent_Declare(
+  vcpkg
+  GIT_REPOSITORY git@github.com:microsoft/vcpkg.git
+)
